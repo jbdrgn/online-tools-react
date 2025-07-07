0e153ea21b33c36fec9764bd2c6f0f7135487dda
jbdrgn <bscdrs@proton.me>
2025-07-07 10:18:05 +0200

    Updates

---
+++ precache-manifest.7b1a34b8bd3c01f7e8e7b387d296548a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4046c294054b636617bf30b381763036",
+    "url": "/online-tools-react/index.html"
+  },
+  {
+    "revision": "b24029264eb30b41ba48",
+    "url": "/online-tools-react/static/css/2.a63ca25f.chunk.css"
+  },
+  {
+    "revision": "51e59d3b9ef4ef4a1605",
+    "url": "/online-tools-react/static/css/main.cb669b7f.chunk.css"
+  },
+  {
+    "revision": "b24029264eb30b41ba48",
+    "url": "/online-tools-react/static/js/2.c50a7c61.chunk.js"
+  },
+  {
+    "revision": "dc6a78df6b1825b5f66661e873789e9a",
+    "url": "/online-tools-react/static/js/2.c50a7c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e59d3b9ef4ef4a1605",
+    "url": "/online-tools-react/static/js/main.fd82852f.chunk.js"
+  },
+  {
+    "revision": "3d7a16dd27ff259cee2c",
+    "url": "/online-tools-react/static/js/runtime-main.e8587c89.js"
+  },
+  {
+    "revision": "bf09acc3b984b16ace2b2b8fe03d1354",
+    "url": "/online-tools-react/static/media/logo.bf09acc3.svg"
+  }
+]);
